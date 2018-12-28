862a0916d64370590ab3c76c6a3e87989c378416
manjusha7671 <manjusha_7671@yahoo.com>
2018-12-28 17:24:46 +0530

    kkk

--- src/main/java/mavenDemo/stepDefinition/LoginStepDefinition.java
+++ src/main/java/mavenDemo/stepDefinition/LoginStepDefinition.java
@@ -16,10 +16,6 @@ public class LoginStepDefinition extends TestBase {
 	@Given("^Iam on the Login page URL \"([^\"]*)\"$")
 	public void iam_on_the_Login_page_URL(String url) throws Throwable {
 		driver.get(url);
-		System.out.println("hi");
-
-
-
 	}
 
 	@And("^I click on the signIn button$")
